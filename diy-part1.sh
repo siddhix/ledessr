cba2b99e3e65bb0db13fed30d6b8c67ef53a097f
siddhix <41867661+siddhix@users.noreply.github.com>
2026-05-05 17:22:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 添加额外的插件源（补全 naiveproxy 等缺失依赖）
-sed -i '$a src-git helloworld https://github.com/fw8719/helloworld' feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw8719/helloworld' feeds.conf.default
 
 # 更新并安装
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
